22613d0c88e8734ab01edad43e28ac28ad055a5d
MEDSTAT <jeanlecot@jean-lecots-mbp.lan>
2014-09-09 20:58:10 +0100

    first try

---
+++ R/multiply.R
@@ -0,0 +1,11 @@
+#' Multiply together two numbers.
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The product of \code{x} and \code{y}.
+#' @examples
+#' mult(1, 2)
+#' mult(10, 1/10)
+mult <- function(x, y) {
+  x * y
+}
