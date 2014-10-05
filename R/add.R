c781b674458ab0d37247e1450763aad5d5887e9f
Jean Lecot <jrm.lecot@gmail.com>
2014-10-05 08:59:22 +0100

    add export

--- R/add.R
+++ R/add.R
@@ -4,6 +4,7 @@
 #' @param x A number.
 #' @param y A number.
 #' @return The sum of \code{x} and \code{y}
+#' @export
 add <- function(x, y) {
   x + y
 }
--- R/multiply.R
+++ R/multiply.R
@@ -3,6 +3,7 @@
 #' @param x A number.
 #' @param y A number.
 #' @return The product of \code{x} and \code{y}.
+#' @export
 mult <- function(x, y) {
   x * y
 }
